5e9d1e7d3891fddf5601370316dcf7d5671206aa
UltimateDesignSlayer <farhadinho99@googlemail.com>
2016-04-18 10:59:48 +0100

    Updates to setting this up

---
+++ js/master.js
@@ -0,0 +1,21 @@
+var master = (function(){
+
+  /**
+   * TODO::::
+   * Call in colour component when in colour section.
+   *
+   * Call in colour component + the component of the section your in. This is
+   * cos we're going to use colours in them.
+   *
+   * Call these in using node
+   */
+
+   
+
+
+   return {
+      test: function(){
+
+      }
+   };
+})();
